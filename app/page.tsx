ab57129f125e89b2ce866305c62bb08f984668be
developersung13 <developersung13@gmail.com>
2024-03-24 22:50:30 +0900

    [fix]: 메인페이지 내 스크롤 버튼 추가 (#23)

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,20 @@ import SwiperBanner from './components/SwiperBanner';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import main_1Img from '@/public/images/main_1.png';
 import main_2Img from '@/public/images/main_2.png';
 import main_3Img from '@/public/images/main_3.png';
 import Link from 'next/link';
 
 export default function Home() {
+  const scrollRef: any = useRef(null);
+
+  const handleButtonClick = () => {
+    // 해당 요소로 스크롤
+    scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   useEffect(() => {
     AOS.init();
   }, []);
@@ -26,6 +33,22 @@ export default function Home() {
       >
         😆 버디 시작하기
       </button> */}
+
+      <button className='mt-4' onClick={handleButtonClick}>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          height='67.5'
+          viewBox='0 -960 960 960'
+          width='67.5'
+          fill='#939baa'
+          className='animate-[bounce_1.5s_infinite]'
+        >
+          <path d='M480-385.077q-6.462 0-11.923-2.115-5.462-2.116-10.692-7.346L281.846-570.077q-5.615-5.615-6-13.769-.385-8.154 6-14.539 6.385-6.384 14.154-6.384t14.154 6.384L480-428.539l169.846-169.846q5.615-5.615 13.769-6 8.154-.384 14.539 6 6.385 6.385 6.385 14.154 0 7.77-6.385 14.154L502.615-394.538q-5.23 5.23-10.692 7.346-5.461 2.115-11.923 2.115Z' />
+        </svg>
+      </button>
+
+      <div className='mb-[-5.5rem]' ref={scrollRef} />
+
       <div className='w-[55rem] flex justify-between items-start mt-[10rem]'>
         <div
           className='flex flex-col justify-center gap-6 mt-20'
