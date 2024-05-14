4fa5649fbc3b750db0b62975977d745fbf5afd21
developersung13 <developersung13@gmail.com>
2024-05-14 22:59:25 +0900

    [feat]: 파티 만들기 상세 내용 저장 store 추가 (#34)

---
+++ app/store/party/AddPartyInfo.tsx
@@ -0,0 +1,91 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+export interface AddPartyInfoState {
+  partyInfo: {
+    stepName: string;
+    planName: string;
+    accountInfo: { id: string; password: string };
+    recruitmentNum: number;
+    startDate: string;
+    durationMonth: number;
+    endDate: string;
+  };
+}
+
+const initialPartyInfoState = {
+  stepName: 'inputAccount',
+  planName: '넷플릭스 스탠다드',
+  accountInfo: { id: '', password: '' },
+  recruitmentNum: 0,
+  startDate: '',
+  durationMonth: 0,
+  endDate: '',
+};
+
+export const AddPartyInfoStore = create(
+  devtools((set) => ({
+    partyInfo: initialPartyInfoState,
+
+    updateStepName: (newStepName: string) =>
+      set((state: AddPartyInfoState) => ({
+        partyInfo: {
+          ...state.partyInfo,
+          stepName: newStepName,
+        },
+      })),
+
+    updatePlanName: (newPlanName: string) =>
+      set((state: AddPartyInfoState) => ({
+        partyInfo: {
+          ...state.partyInfo,
+          planName: newPlanName,
+        },
+      })),
+
+    updateAccountInfo: (newAccountInfo: { id: string; password: string }) =>
+      set((state: AddPartyInfoState) => ({
+        partyInfo: {
+          ...state.partyInfo,
+          accountInfo: { ...state.partyInfo.accountInfo, ...newAccountInfo },
+        },
+      })),
+
+    updateRecruitmentNum: (newRecruitmentNum: number) =>
+      set((state: AddPartyInfoState) => ({
+        partyInfo: {
+          ...state.partyInfo,
+          recruitmentNum: newRecruitmentNum,
+        },
+      })),
+
+    updateStartDate: (newStartDate: Date) =>
+      set((state: AddPartyInfoState) => ({
+        partyInfo: {
+          ...state.partyInfo,
+          startDate: newStartDate,
+        },
+      })),
+
+    updateDurationMonth: (newDurationMonth: number) =>
+      set((state: AddPartyInfoState) => ({
+        partyInfo: {
+          ...state.partyInfo,
+          durationMonth: newDurationMonth,
+        },
+      })),
+
+    updateEndDate: (newEndDate: Date) =>
+      set((state: AddPartyInfoState) => ({
+        partyInfo: {
+          ...state.partyInfo,
+          endDate: newEndDate,
+        },
+      })),
+
+    resetPartyInfo: () =>
+      set(() => ({
+        partyInfo: initialPartyInfoState,
+      })),
+  }))
+);
