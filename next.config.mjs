2e84f3f45632164364a9888e87a58f125c9ac44c
developersung13 <developersung13@gmail.com>
2024-07-24 20:00:00 +0900

    [feat] Image 컴포넌트의 외부 이미지 파일 접근 허용을 위한 next.config 파일 수정 #42

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
