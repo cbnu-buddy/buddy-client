ae3fcc2d4ebb01bbd8c6a900c4e865cf8a09fc46
developersung13 <developersung13@gmail.com>
2024-08-10 14:03:18 +0900

    [feat] isToastClosing state명 정정 #49

--- app/community/my/subscribed-tags/page.tsx
+++ app/community/my/subscribed-tags/page.tsx
@@ -207,9 +207,7 @@ export default function MySubscribedTags() {
       {(isOpenSubscribeCompleteToast || isOpenUnSubscribeCompleteToast) && (
         <Toast
           className={`w-fit fixed bottom-14 left-14 bg-[#222222] py-3 pr-5 ${
-            isUnSubscribeCompleteToastClosing
-              ? 'toast-fade-out'
-              : 'toast-fade-in'
+            isToastClosing ? 'toast-fade-out' : 'toast-fade-in'
           }`}
         >
           <div className='inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg text-green-500 dark:bg-green-800 dark:text-green-200'>
