330e0e385008e7a0798b9937c6b7fdd5cb5954fd
developersung13 <developersung13@gmail.com>
2024-08-25 12:50:30 +0900

    [feat] 전역 레이아웃 내 Toast 컴포넌트 추가 #60

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Toast } from 'flowbite-react';
+import Toast from './components/Toast';
 import Navbar from './components/Navbar';
 import './globals.css';
 import Providers from './utils/Providers';
@@ -21,26 +21,7 @@ export default function RootLayout({
       <body className='text-sm'>
         <Providers>
           <Navbar />
-          {/* <Toast
-            className={`w-fit fixed bottom-14 left-14 bg-[#222222] py-3 pr-5 ${
-              isToastClosing ? 'toast-fade-out' : 'toast-fade-in'
-            }`}
-          >
-            <div className='inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg text-green-500 dark:bg-green-800 dark:text-green-200'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                height='27.5px'
-                viewBox='0 -960 960 960'
-                width='27.5px'
-                fill='#75d5ad'
-              >
-                <path d='m382-354 339-339q12-12 28-12t28 12q12 12 12 28.5T777-636L410-268q-12 12-28 12t-28-12L182-440q-12-12-11.5-28.5T183-497q12-12 28.5-12t28.5 12l142 143Z' />
-              </svg>
-            </div>
-            <div className='ml-1 text-[0.825rem] font-medium text-white'>
-              
-            </div>
-          </Toast> */}
+          <Toast />
           <main className='w-full mx-auto pt-[3.75rem]'>{children}</main>
         </Providers>
       </body>
