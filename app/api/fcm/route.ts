a44bd3bdf00623805987de3c1145a5c8bedb51bb
developersung13 <developersung13@gmail.com>
2024-08-26 03:07:01 +0900

    [feat] private_key 줄바꿈 처리 코드 추가 #60

--- app/api/fcm/route.ts
+++ app/api/fcm/route.ts
@@ -21,7 +21,10 @@ const firebaseConfig: FirebaseConfig = {
   type: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_TYPE,
   project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PROJECT_ID,
   private_key_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY,
+  private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY?.replace(
+    /\\n/g,
+    '\n'
+  ),
   client_email: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
   client_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_ID,
   auth_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_AUTH_URI,
