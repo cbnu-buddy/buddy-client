30ee680fb4848ddc29abafeac80c9830ffccc51e
developersung13 <developersung13@gmail.com>
2024-08-25 23:43:33 +0900

    [feat] firebase-admin-sdk 파일 제거로 인한 firebaseConfig 데이터 직접 추가 #60

--- app/api/fcm/route.ts
+++ app/api/fcm/route.ts
@@ -1,9 +1,37 @@
 import { NextResponse } from 'next/server';
-import admin from 'firebase-admin';
+import admin, { ServiceAccount } from 'firebase-admin';
 import crypto from 'crypto';
 import { connectDB } from '@/config/dbConnection';
 
-const firebaseConfig = require('@/constants/fcm/firebase-admin-sdk.json');
+interface FirebaseConfig extends ServiceAccount {
+  type: string | undefined;
+  project_id: string | undefined;
+  private_key_id: string | undefined;
+  private_key: string | undefined;
+  client_email: string | undefined;
+  client_id: string | undefined;
+  auth_uri: string | undefined;
+  token_uri: string | undefined;
+  auth_provider_x509_cert_url: string | undefined;
+  client_x509_cert_url: string | undefined;
+  universe_domain: string | undefined;
+}
+
+const firebaseConfig: FirebaseConfig = {
+  type: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_TYPE,
+  project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PROJECT_ID,
+  private_key_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY,
+  client_email: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_ID,
+  auth_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_AUTH_URI,
+  token_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_TOKEN_URI,
+  auth_provider_x509_cert_url:
+    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url:
+    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_UNIVERSE_DOMAIN,
+};
 
 if (admin.apps.length === 0) {
   admin.initializeApp({
