1492a2913879c28ad6862b79fadd5321ab3167a9
developersung13 <developersung13@gmail.com>
2024-09-01 01:10:49 +0900

    [feat] 커뮤니티 게시글 수정 페이지 컴포넌트 추가 #68

--- app/community/post/[postId]/edit/page.tsx
+++ app/community/post/[postId]/edit/page.tsx
@@ -1,6 +1,290 @@
-import Developing from '@/app/components/Developing';
-import React from 'react';
+'use client';
 
-export default function page() {
-  return <Developing />;
+import Loading from '@/app/loading';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import React, { useRef, useState, useEffect } from 'react';
+import TagEditor from './components/TagEditor';
+import LinkedServicesModal from './components/LinkedServicesModal';
+import { partySelectedPlanInfos } from '@/data/partySelectedPlanInfos';
+import Link from 'next/link';
+import Image from 'next/image';
+import { PostInfo } from '@/types/post';
+import { TagInfo } from '@/types/tag';
+import CustomCKEditor from '@/app/components/CustomCKEditor';
+
+// const CustomCKEditor = dynamic(
+//   () => import('@/app/components/CustomCKEditor'),
+//   { ssr: false, loading: () => <Loading /> }
+// );
+
+export default function EditCommunityPost() {
+  const resData: PostInfo = {
+    postId: 18,
+    title: '여행 가기전 보기 좋은 방송 프로그램',
+    content:
+      '<p>이건 버디 커뮤니티 게시글 본문 <strong>테스트 내용</strong>입니다.</p><p>&nbsp;</p><figure class="image"><img src="https://swjudgeapi.cbnu.ac.kr/uploads/c24e87aa-a99d-479e-a134-c7f4554291cf.png"></figure><p>&nbsp;</p><p>게시글 하단 내용도 포함</p>',
+    createdAt: '2024-08-08T19:12:19',
+    modifiedAt: null,
+    postImagePathUrls: ['string'],
+    author: {
+      memberId: 2,
+      username: '똑똑한 쿠키',
+      profileImagePathUrl:
+        'https://avatars.githubusercontent.com/u/56868605?s=400&u=f10a6f2a966512a6ec9983df037141cfba5faf31&v=4',
+    },
+    tags: [
+      {
+        tagId: 14,
+        tag: '애니메이션',
+      },
+      {
+        tagId: 14,
+        tag: '감자',
+      },
+    ],
+    views: 4,
+    services: [
+      {
+        serviceId: 1,
+        planIds: [1, 2],
+        name: '넷플릭스',
+        url: 'https://www.netflix.com',
+      },
+      {
+        serviceId: 2,
+        planIds: [3, 4],
+        name: '웨이브',
+        url: 'https://www.wavve.com',
+      },
+    ],
+    comments: [
+      {
+        commentId: 4,
+        commentContent: 'string',
+        likeCount: 0,
+        createdAt: '2024-08-11T13:49:57',
+        writer: {
+          memberId: 2,
+          username: '똑똑한 쿠키',
+          profileImagePathUrl:
+            'https://avatars.githubusercontent.com/u/56868605?s=400&u=f10a6f2a966512a6ec9983df037141cfba5faf31&v=4',
+        },
+        replies: [
+          {
+            replyId: 4,
+            replyContent: 'string2',
+            likeCount: 1,
+            createdAt: '2024-08-11T13:50:17',
+            writer: {
+              memberId: 2,
+              username: '똑똑한 쿠키',
+              profileImagePathUrl:
+                'https://avatars.githubusercontent.com/u/56868605?s=400&u=f10a6f2a966512a6ec9983df037141cfba5faf31&v=4',
+            },
+          },
+        ],
+      },
+    ],
+    likeCount: 0,
+  };
+
+  const postInfo: PostInfo = resData;
+
+  const [title, setTitle] = useState('');
+  const [isTitleValidFail, setIsTitleValidFail] = useState(false);
+  const [content, setContent] = useState('');
+  const [tagList, setTagList] = useState<string[]>([]);
+  const [openLinkedServicesModal, setOpenLinkedServicesModal] = useState<
+    string | undefined
+  >();
+  const [selectedServices, setSelectedServices] = useState<number[]>([]);
+
+  const titleRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (postInfo) {
+      setTitle(postInfo.title);
+      setContent(postInfo.content);
+      const tags = postInfo.tags.map((tagInfo: TagInfo) => tagInfo.tag);
+      setTagList(tags);
+      const serviceIds = postInfo.services.map(
+        (serviceInfo) => serviceInfo.serviceId
+      );
+      setSelectedServices(serviceIds);
+    }
+  }, [postInfo]);
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+    setIsTitleValidFail(false);
+  };
+
+  const handleCancelEditPost = () => {
+    const userResponse = confirm('게시글 수정을 취소하시겠습니까?');
+    if (!userResponse) return;
+
+    router.back();
+  };
+
+  const handleEditPost = () => {
+    if (!title) {
+      alert('제목을 입력해 주세요');
+      window.scrollTo(0, 0);
+      titleRef.current?.focus();
+      setIsTitleValidFail(true);
+      return;
+    }
+
+    if (content === null) {
+      alert('본문을 입력해 주세요');
+      window.scrollTo(0, 0);
+      return;
+    }
+
+    router.push(`/community/post/${1}`);
+  };
+
+  if (!content) return <Loading />;
+
+  return (
+    <div className='mt-6 mb-4 px-5 2lg:px-0'>
+      <div className='flex flex-col w-[50rem] mx-auto'>
+        <p className='text-2xl font-semibold'>글 쓰기</p>
+        <div className='flex flex-col relative z-0 w-1/2 group mt-5 mb-8'>
+          <input
+            type='text'
+            name='floating_first_name'
+            className={`block pt-3 pb-[0.175rem] pl-0 pr-0 w-full font-normal text-gray-900 bg-transparent border-0 border-b border-gray-400 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-${
+              isTitleValidFail ? 'pink' : 'blue'
+            }-500 focus:border-${
+              isTitleValidFail ? 'red' : 'blue'
+            }-500 focus:outline-none focus:ring-0 peer`}
+            placeholder=' '
+            required
+            value={title}
+            ref={titleRef}
+            onChange={handleTitleChange}
+          />
+          <label
+            htmlFor='floating_first_name'
+            className={`peer-focus:font-light absolute text-base left-[0.1rem] font-light text-${
+              isTitleValidFail ? 'red' : 'gray'
+            }-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-3 -z-10 origin-[0] peer-focus:left-[0.1rem] peer-focus:text-${
+              isTitleValidFail ? 'red' : 'blue'
+            }-600 peer-focus:dark:text-${
+              isTitleValidFail ? 'red' : 'blue'
+            }-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-[1.25rem]`}
+          >
+            게시글 제목
+          </label>
+          <p
+            className={`text-${
+              isTitleValidFail ? 'red' : 'gray'
+            }-500 text-xs tracking-widest font-light mt-1`}
+          >
+            제목을 입력해 주세요
+          </p>
+        </div>
+
+        <div className='w-full mx-auto overflow-auto'>
+          <CustomCKEditor
+            initEditorContent={content}
+            onEditorChange={setContent}
+          />
+        </div>
+
+        <div className='mt-8 flex flex-col'>
+          <div className='flex flex-col gap-y-10'>
+            <div className='flex flex-col gap-y-1'>
+              <p className='text-base font-semibold'>태그</p>
+              <TagEditor tagList={tagList} setTagList={setTagList} />
+            </div>
+
+            <div className='flex flex-col items-start gap-y-[0.375rem]'>
+              <div className='flex gap-x-4 items-center'>
+                <p className='text-base font-semibold'>연결할 서비스</p>
+                <button
+                  onClick={() => {
+                    setOpenLinkedServicesModal('default');
+                  }}
+                  className='flex gap-1 bg-[#f2f4f6] text-[#4e5968] px-2 py-[0.375rem] rounded-[0.25rem] font-sm focus:bg-[#dbe0e5] hover:bg-[#dbe0e5]'
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    height='18'
+                    viewBox='0 -960 960 960'
+                    width='18'
+                    fill='#4e5968'
+                    className='relative top-[0.125rem]'
+                  >
+                    <path d='M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z' />
+                  </svg>
+                  조회하기
+                </button>
+              </div>
+
+              <div className='mt-2 flex gap-x-3 text-[#333d4b]'>
+                {selectedServices.map((selectedServiceId, index) => (
+                  <Link
+                    key={index}
+                    href={`/join-party/plan/${
+                      partySelectedPlanInfos[selectedServiceId - 1]
+                        .planDetailInfos[
+                        partySelectedPlanInfos[selectedServiceId - 1]
+                          .planDetailInfos.length - 1
+                      ].id
+                    }`}
+                    className='w-[5.75rem] text-center flex flex-col items-center bg-[#f2f4f6] px-4 pb-3 rounded-lg hover:bg-[#dbe0e5]'
+                  >
+                    <Image
+                      src={
+                        partySelectedPlanInfos[selectedServiceId - 1].iconImg
+                      }
+                      alt={
+                        partySelectedPlanInfos[selectedServiceId - 1].iconImgAlt
+                      }
+                      width={60}
+                      height={0}
+                      quality={100}
+                      className='mt-3'
+                    />
+
+                    <p className='mt-2 text-inherit text-[0.825rem] font-medium'>
+                      {partySelectedPlanInfos[selectedServiceId - 1].name}
+                    </p>
+                  </Link>
+                ))}
+              </div>
+
+              {openLinkedServicesModal && (
+                <LinkedServicesModal
+                  openLinkedServicesModal={openLinkedServicesModal}
+                  setOpenLinkedServicesModal={setOpenLinkedServicesModal}
+                  selectedServices={selectedServices}
+                  setSelectedServices={setSelectedServices}
+                />
+              )}
+            </div>
+          </div>
+
+          <div className='mt-14 pb-2 flex justify-end gap-3'>
+            <button
+              onClick={handleCancelEditPost}
+              className='px-4 py-[0.5rem] rounded-[6px] font-light'
+            >
+              취소
+            </button>
+            <button
+              onClick={handleEditPost}
+              className='text-[#f9fafb] bg-[#3a8af9] px-4 py-[0.5rem] rounded-[6px] focus:bg-[#1c6cdb] hover:bg-[#1c6cdb]'
+            >
+              수정
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
